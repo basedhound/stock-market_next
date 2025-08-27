b32e2b0f9130471c4fd88431cbcfa2ac2ba5a14a
fvukelic <franck.vukelic@gmail.com>
2025-08-27 19:39:35 +0200

    Setup Database

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,17 @@
-const Home = () => {
+import { testDbConnection } from "@/lib/actions/test.actions";
+
+const Home = async () => {
+  const response = await testDbConnection();
   return (
-    <div className='flex min-h-screen home-wrapper'>
-      <section className='w-full gap-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
-        <div className='md:col-span-1 xl:col-span-1'>MarketSummary</div>
-        <div className='md:col-span-1 xl:col-span-2'>Heatmap</div>
+    <div className="flex min-h-screen home-wrapper">
+      <p>{response.data} to database</p>
+      <section className="w-full gap-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+        <div className="md:col-span-1 xl:col-span-1">MarketSummary</div>
+        <div className="md:col-span-1 xl:col-span-2">Heatmap</div>
       </section>
-      <section className='w-full gap-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
-        <div className='h-full md:col-span-1 xl:col-span-1'>TopStories</div>
-        <div className='h-full md:col-span-1 xl:col-span-2'>
+      <section className="w-full gap-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+        <div className="h-full md:col-span-1 xl:col-span-1">TopStories</div>
+        <div className="h-full md:col-span-1 xl:col-span-2">
           Market Performance
         </div>
       </section>
