d4abbafcbfa17bfba660665a6d4b83ecb89ffa35
fvukelic <franck.vukelic@gmail.com>
2025-08-27 16:06:56 +0200

    Add Homepage & Header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <div className='flex min-h-screen home-wrapper'>
+      <section className='w-full gap-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
+        <div className='md:col-span-1 xl:col-span-1'>MarketSummary</div>
+        <div className='md:col-span-1 xl:col-span-2'>Heatmap</div>
+      </section>
+      <section className='w-full gap-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
+        <div className='h-full md:col-span-1 xl:col-span-1'>TopStories</div>
+        <div className='h-full md:col-span-1 xl:col-span-2'>
+          Market Performance
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
