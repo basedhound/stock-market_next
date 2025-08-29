fbbc4414c4f627fe90ef5aa9445d1e958afc1819
fvukelic <franck.vukelic@gmail.com>
2025-08-29 14:58:45 +0200

    Update types

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,5 @@
 declare global {
-  interface SignInFormData {
+  type SignInFormData = {
     email: string;
     password: string;
   }
@@ -14,16 +14,87 @@ declare global {
     preferredIndustry: string;
   };
 
-  interface WelcomeEmailData {
+  type CountrySelectProps = {
+    name: string;
+    label: string;
+    control: Control;
+    error?: FieldError;
+    required?: boolean;
+  };
+
+  type FormInputProps = {
+    name: string;
+    label: string;
+    placeholder: string;
+    type?: string;
+    register: UseFormRegister;
+    error?: FieldError;
+    validation?: RegisterOptions;
+    disabled?: boolean;
+    value?: string;
+  };
+
+  type Option = {
+    value: string;
+    label: string;
+  };
+
+  type SelectFieldProps = {
+    name: string;
+    label: string;
+    placeholder: string;
+    options: readonly Option[];
+    control: Control;
+    error?: FieldError;
+    required?: boolean;
+  };
+
+  type FooterLinkProps = {
+    text: string;
+    linkText: string;
+    href: string;
+  };
+
+  type SearchCommandProps = {
+    renderAs?: 'button' | 'text';
+    label?: string;
+    initialStocks: StockWithWatchlistStatus[];
+  };
+
+  type WelcomeEmailData = {
     email: string;
     name: string;
     intro: string;
-  }
+  };
 
   type User = {
     id: string;
     name: string;
     email: string;
+  };
+
+  type Stock = {
+    symbol: string;
+    name: string;
+    exchange: string;
+    type: string;
+  };
+
+  type StockWithWatchlistStatus = Stock & {
+    isInWatchlist: boolean;
+  };
+
+  type FinnhubSearchResult = {
+    symbol: string;
+    description: string;
+    displaySymbol?: string;
+    type: string;
+    exchange: string;
+  };
+
+  type FinnhubSearchResponse = {
+    count: number;
+    result: FinnhubSearchResult[];
   };
 
   //
@@ -183,17 +254,6 @@ declare global {
     open: boolean;
   };
 
-  type Stock = {
-    symbol: string;
-    name: string;
-    exchange: string;
-    type: string;
-  };
-
-  type StockWithWatchlistStatus = Stock & {
-    isInWatchlist: boolean;
-  };
-
   type UserForNewsEmail = {
     id: string;
     email: string;
@@ -231,13 +291,6 @@ declare global {
     marketCap?: string;
     peRatio?: string;
     tradingViewSymbol?: string;
-  };
-
-  type FinnhubSearchResult = {
-    symbol: string;
-    description: string;
-    displaySymbol?: string;
-    type: string;
   };
 }
 
